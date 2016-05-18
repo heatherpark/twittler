458d714365ab9d471c5b1ae2814ddbe1824a3a3f
Heather Park <hthr.prk@gmail.com>
2016-05-18 09:14:32 -0700

    Add feed div to index.html and establish general plan for loadTweets() function in feed.js

--- feed.js
+++ feed.js
@@ -1,7 +1,24 @@
 $(document).ready(function(){
   // new tweets button
 
-  // load all user tweets
+  // LOAD ALL USER TWEETS
+
+  // feed that displays all tweets from all users.
+  function loadTweets(index) {
+
+    var $feed = (".feed");
+
+    // use loop to create HTML setup for each tweet.
+    $.each(streams.home, function(index, value) {
+      // establish HTML element for each tweet component.
+
+      // assign appropriate text for various tweet components using
+      // "tweet" object in data_generator.js.
+
+      // prepend tweets to $feed in order for them to be in reverse chronological order.
+    });
+
+  }
 
   // load user-specific tweets
 
